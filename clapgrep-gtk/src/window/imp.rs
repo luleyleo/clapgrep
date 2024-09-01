41c7531f8fdd253fb2b98b3ebc0238440bd4b8e5
Leopold Luley <git@leopoldluley.de>
2024-09-01 21:00:41 +0200

    gtk: Add bindings for search fields.

--- clapgrep-gtk/src/window/imp.rs
+++ clapgrep-gtk/src/window/imp.rs
@@ -1,13 +1,17 @@
-use std::cell::Cell;
-
 use adw::subclass::prelude::*;
+use glib::prelude::*;
 use glib::subclass::InitializingObject;
 use gtk::{glib, CompositeTemplate};
+use std::cell::RefCell;
 
-#[derive(CompositeTemplate, Default)]
+#[derive(CompositeTemplate, glib::Properties, Default)]
 #[template(file = "src/window/window.blp")]
+#[properties(wrapper_type = super::Window)]
 pub struct Window {
-    pub number: Cell<i32>,
+    #[property(get, set)]
+    pub file_search: RefCell<String>,
+    #[property(get, set)]
+    pub content_search: RefCell<String>,
 }
 
 #[glib::object_subclass]
@@ -27,8 +31,15 @@ impl ObjectSubclass for Window {
 }
 
 #[gtk::template_callbacks]
-impl Window {}
+impl Window {
+    #[template_callback]
+    fn on_search_button_clicked(&self, _: &gtk::Button) {
+        println!("file_search = {}", self.file_search.borrow());
+        println!("content_search = {}", self.content_search.borrow());
+    }
+}
 
+#[glib::derived_properties]
 impl ObjectImpl for Window {}
 
 impl WidgetImpl for Window {}
