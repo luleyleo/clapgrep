a61cba4adb322f4a8dd8fe5a78a647e7b8a293a4
Leopold Luley <git@leopoldluley.de>
2024-09-05 14:47:17 +0200

    gnome: Support opening directories from file browser.

--- gnome/src/main.rs
+++ gnome/src/main.rs
@@ -1,4 +1,5 @@
 use adw::prelude::*;
+use gtk::gio::{self, ApplicationFlags};
 use gtk::glib::{self, clone};
 use gtk::{gio::SimpleAction, License};
 use gtk_blueprint::include_blp;
@@ -30,65 +31,77 @@ fn main() {
 
     let app = adw::Application::builder()
         .application_id("de.leopoldluley.Clapgrep")
+        .flags(ApplicationFlags::HANDLES_OPEN)
         .build();
 
-    app.connect_activate(|app| {
-        let app = app.downcast_ref::<adw::Application>().unwrap();
-        let window = search_window::SearchWindow::new(app);
-
-        let about_action = SimpleAction::new("about", None);
-        about_action.connect_activate(clone!(
-            #[weak]
-            window,
-            move |_, _| {
-                adw::AboutDialog::builder()
-                    .application_name("Clapgrep")
-                    .version("0.1")
-                    .application_icon("de.leopoldluley.Clapgrep")
-                    .developer_name("Leopold Luley")
-                    .website("https://github.com/luleyleo/clapgrep")
-                    .issue_url("https://github.com/luleyleo/clapgrep/issues")
-                    .license_type(License::Gpl30)
-                    .copyright("© 2024 Leopold Luley")
-                    .build()
-                    .present(Some(&window));
-            }
-        ));
-        app.add_action(&about_action);
-
-        let shortcuts_action = SimpleAction::new("shortcuts", None);
-        shortcuts_action.connect_activate(clone!(
-            #[weak]
-            window,
-            move |_, _| {
-                let builder = gtk::Builder::from_string(include_blp!("gnome/src/shortcuts.blp"));
-                let help_overlay = builder
-                    .object::<gtk::ShortcutsWindow>("help-overlay")
-                    .unwrap();
-                help_overlay.set_transient_for(Some(&window));
-                help_overlay.set_application(window.application().as_ref());
-                help_overlay.present();
-            }
-        ));
-        app.add_action(&shortcuts_action);
-
-        let quit_action = SimpleAction::new("quit", None);
-        quit_action.connect_activate(clone!(
-            #[weak]
-            window,
-            move |_, _| {
-                window.close();
-            }
-        ));
-        app.add_action(&quit_action);
-
-        app.set_accels_for_action("app.quit", &["<ctrl>q"]);
-        app.set_accels_for_action("app.shortcuts", &["<ctrl>h"]);
-        app.set_accels_for_action("win.start-search", &["<ctrl>Return"]);
-        app.set_accels_for_action("app.stop-search", &["<ctrl>s"]);
-
-        window.present();
-    });
+    app.connect_open(|app, files, _| start(app, files));
+    app.connect_activate(|app| start(app, &[]));
 
     app.run();
+}
+
+fn start(app: &adw::Application, files: &[gio::File]) {
+    let app = app.downcast_ref::<adw::Application>().unwrap();
+    let window = search_window::SearchWindow::new(app);
+
+    if let Some(dir) = files.first() {
+        if let Some(path) = dir.path() {
+            if path.is_dir() {
+                window.set_search_path(path.to_string_lossy());
+            }
+        }
+    }
+
+    let about_action = SimpleAction::new("about", None);
+    about_action.connect_activate(clone!(
+        #[weak]
+        window,
+        move |_, _| {
+            adw::AboutDialog::builder()
+                .application_name("Clapgrep")
+                .version("0.1")
+                .application_icon("de.leopoldluley.Clapgrep")
+                .developer_name("Leopold Luley")
+                .website("https://github.com/luleyleo/clapgrep")
+                .issue_url("https://github.com/luleyleo/clapgrep/issues")
+                .license_type(License::Gpl30)
+                .copyright("© 2024 Leopold Luley")
+                .build()
+                .present(Some(&window));
+        }
+    ));
+    app.add_action(&about_action);
+
+    let shortcuts_action = SimpleAction::new("shortcuts", None);
+    shortcuts_action.connect_activate(clone!(
+        #[weak]
+        window,
+        move |_, _| {
+            let builder = gtk::Builder::from_string(include_blp!("gnome/src/shortcuts.blp"));
+            let help_overlay = builder
+                .object::<gtk::ShortcutsWindow>("help-overlay")
+                .unwrap();
+            help_overlay.set_transient_for(Some(&window));
+            help_overlay.set_application(window.application().as_ref());
+            help_overlay.present();
+        }
+    ));
+    app.add_action(&shortcuts_action);
+
+    let quit_action = SimpleAction::new("quit", None);
+    quit_action.connect_activate(clone!(
+        #[weak]
+        window,
+        move |_, _| {
+            window.close();
+        }
+    ));
+    app.add_action(&quit_action);
+
+    app.set_accels_for_action("app.quit", &["<ctrl>q"]);
+    app.set_accels_for_action("app.shortcuts", &["<ctrl>h"]);
+    app.set_accels_for_action("win.start-search", &["<ctrl>Return"]);
+    app.set_accels_for_action("app.stop-search", &["<ctrl>s"]);
+
+    window.present();
 }
