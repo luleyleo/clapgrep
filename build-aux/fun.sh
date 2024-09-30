d0e8d695d346d20dadd00610bc4b32eadea331c8
Leopold Luley <git@leopoldluley.de>
2024-09-30 23:00:50 +0200

    Build in debug mode and with .Devel suffix by default.

--- build-aux/fun.sh
+++ build-aux/fun.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env sh
 
-flatpak-builder --run $(dirname $0)/../flatpak $(dirname $0)/de.leopoldluley.Clapgrep.json env CARGO_HOME=/run/build/clapgrep/cargo "$@"
+flatpak-builder --run \
+  $(dirname $0)/../flatpak \
+  $(dirname $0)/de.leopoldluley.Clapgrep.json \
+  env CARGO_HOME=/run/build/clapgrep/cargo APP_ID=$APP_ID "$@"
