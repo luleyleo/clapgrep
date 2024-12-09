2217b6ba072d7bbfc8d5d3f08665c5f5451cbe5c
Leopold Luley <git@leopoldluley.de>
2024-12-09 14:38:54 +0100

    Use online builds for development.

--- build-aux/fun.sh
+++ build-aux/fun.sh
@@ -2,5 +2,5 @@
 
 flatpak-builder --run \
   $(dirname $0)/../flatpak \
-  $(dirname $0)/de.leopoldluley.Clapgrep.json \
+  $(dirname $0)/de.leopoldluley.Clapgrep.Devel.json \
   env CARGO_HOME=/run/build/clapgrep/cargo APP_ID=$APP_ID "$@"
