869c8890f13e1d4b57c4826712876d22bd3c5e16
Leopold Luley <git@leopoldluley.de>
2026-02-05 13:21:56 +0100

    Fix CI helper script.

--- build-aux/fun.sh
+++ build-aux/fun.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 
+BUILD_DIR="/run/build/clapgrep"
+COMMAND="cd $BUILD_DIR && env CARGO_HOME=$BUILD_DIR/cargo $@"
+
 flatpak-builder --run \
   $(dirname $0)/../flatpak \
   $(dirname $0)/de.leopoldluley.Clapgrep.CI.json \
-  env CARGO_HOME=/run/build/clapgrep/cargo "$@"
+  bash -c "$COMMAND"
